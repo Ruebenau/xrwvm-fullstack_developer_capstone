c21ede2d75d26596f7f5f200a172db272dc44023
Aaron Nau <aaronnau2002@gmail.com>
2026-01-14 12:47:15 -0500

    Python Code Standards Compliance #4

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -29,15 +29,15 @@ SECRET_KEY =\
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost',
-                    ('https://aaronnau2002-8000.theianext-0-'
+                ('https://aaronnau2002-8000.theianext-0-'
                     'labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai'),
-                    ('https://aaronnau2002-8000.theiadockernext-0-'
+                ('https://aaronnau2002-8000.theiadockernext-0-'
                     'labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai')]
 CSRF_TRUSTED_ORIGINS = [
                         ('https://aaronnau2002-8000.theianext-0-'
-                        'labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai'),
+                            'labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai'),
                         ('https://aaronnau2002-8000.theiadockernext-0-'
-                        'labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai')]
+                            'labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai')]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
